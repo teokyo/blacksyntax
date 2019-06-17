a4370513167717f65e64b772ff134c4a584b12ad
MarxBlite <olivergarciaw7.0@gmail.com>
2019-06-17 15:57:25 -0500

    Imagen de prueba eliminada

--- blacksyntaxweb/src/Componentes/Home.js
+++ blacksyntaxweb/src/Componentes/Home.js
@@ -6,7 +6,7 @@ import '../App.css';
 class Home extends Component {
     ListaItem = [
         {
-            imagen: "http://lorempixel.com/700/500",
+            imagen: imagen,
             Titulo: 'Titulo 1',
             Subtitulo: 'Subtitulo 1'
         },
