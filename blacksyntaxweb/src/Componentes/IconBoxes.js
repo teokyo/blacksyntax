de6179489d6652789b891fe9d7bd88d1e38a2264
MarxBlite <olivergarciaw7.0@gmail.com>
2019-06-20 21:06:17 -0500

    cambios en master

--- blacksyntaxweb/src/Componentes/IconBoxes.js
+++ blacksyntaxweb/src/Componentes/IconBoxes.js
@@ -13,25 +13,22 @@ class IconBoxes extends Component {
             <div className="icon_boxes">{/* Icon Boxes */}
                 <div className="container">
                     <div className="row">
-                        <div className="col-lg-4">
+                        <div className="col-sm-12  col-lg-4">
                             <div className="icon_box_title">
-                                <h1>Te acercamos al futuro</h1>
-                            </div>
-                            <div className="button icon_box_button trans_200">
-                                <a href="/hola" className="trans_200">mas información</a>
+                                <h1>Dataware House</h1>
                             </div>
                         </div>
         
-                        <div className="col-lg-4 icon_box_col">
+                        <div className="col-sm-12  col-lg-4 icon_box_col">
         
                              {/* Icon Box Item */}
-                            <div className="icon_box_item">
+                            <div className="col-sm-12  col-lg-12 icon_box_item">
                                 <h2>Misión</h2>
                                 <p>Desarrolar ideas innovadoras que ayuden a mejorar el crecimiento de las empresas y a su vez que contribuyan con la economía nacional basados en los principios y valores que nos rigen.</p>
                             </div>
         
                             {/* Icon Box Item */}
-                            <div className="icon_box_item">
+                            <div className="col-sm-12 col-lg-12 icon_box_item">
                                 <h2>valores</h2>
                                 <ul>
                                     <li>responsabilidad</li>
@@ -42,19 +39,19 @@ class IconBoxes extends Component {
         
                         </div>
         
-                        <div className="col-lg-4 icon_box_col">
+                        <div className="col-sm-12 col-lg-4 icon_box_col">
         
                             {/* Icon Box Item */}
-                            <div className="icon_box_item">
+                            <div className="col-sm-12  col-lg-12 icon_box_item">
                                 <h2>Visión</h2>
                                 <p>Ser una empresa de soluciones tecnológicas reconocida a nivel nacional por la calidad e innovación de nuestros servicios y productos desarrollados.</p>
                             </div>
 
                             {/* Icon Box Item */}
-                            {/*<div className="icon_box_item">
+                            <div className="col-sm-12  col-lg-12 icon_box_item">
                                 <h2>Uso</h2>
                                 <p>Etiam euismod sapien nec est tempus consequat. Nullam eget pretium mi. Curabitur rhoncus finibus quam quis sodales.</p>
-        </div>*/}
+        </div>
                             
                         </div>
                     </div>
