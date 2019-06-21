c8eac8864fca77f1873ef3e5e8b2a963b3351873
MarxBlite <olivergarciaw7.0@gmail.com>
2019-06-20 21:17:24 -0500

    cambios en master

--- blacksyntaxweb/src/Subcomponentes/ItemServices.js
+++ blacksyntaxweb/src/Subcomponentes/ItemServices.js
@@ -5,7 +5,37 @@ class ItemServices extends Component {
     state = {};
 
     componentWillMount() {
-        this.setState({ ...(this.props.valores) });
+        this.setState({ ...(this.props.import React, { Component } from 'react';
+
+        class ItemServices extends Component {
+        
+            state = {};
+        
+            componentWillMount() {
+                this.setState({ ...(this.props.valores) });
+            }
+        
+            render() {
+                return (
+                    <div>
+                        <div className=" owl-item services_item">
+                                <div className="services_item_inner">
+                                    <div className="service_item_content">
+                                        <div className="service_item_title">
+                                            <h2>{this.state.Titulo}</h2>
+                                        </div>
+                                        <p>{this.state.parrafo}</p>
+                                    </div>
+                                </div>
+                            </div>
+        
+                    </div>
+        
+                );
+            }
+        }
+        
+        export default ItemServices;valores) });
     }
 
     render() {
