25413729d5de90d3d14c11c34c413470290c9025
MarxBlite <olivergarciaw7.0@gmail.com>
2019-06-20 21:21:01 -0500

    cambios en master

--- blacksyntaxweb/src/Subcomponentes/ItemServices.js
+++ blacksyntaxweb/src/Subcomponentes/ItemServices.js
@@ -5,55 +5,19 @@ class ItemServices extends Component {
     state = {};
 
     componentWillMount() {
-        this.setState({ ...(this.props.import React, { Component } from 'react';
-
-        class ItemServices extends Component {
-        
-            state = {};
-        
-            componentWillMount() {
-                this.setState({ ...(this.props.valores) });
-            }
-        
-            render() {
-                return (
-                    <div>
-                        <div className=" owl-item services_item">
-                                <div className="services_item_inner">
-                                    <div className="service_item_content">
-                                        <div className="service_item_title">
-                                            <h2>{this.state.Titulo}</h2>
-                                        </div>
-                                        <p>{this.state.parrafo}</p>
-                                    </div>
-                                </div>
-                            </div>
-        
-                    </div>
-        
-                );
-            }
-        }
-        
-        export default ItemServices;valores) });
+        this.setState({ ...(this.props.valores) });
     }
 
     render() {
         return (
             <div>
-                <div className="owl-item services_item">
+                <div className=" owl-item services_item">
 						<div className="services_item_inner">
 							<div className="service_item_content">
 								<div className="service_item_title">
-									<div className="service_item_icon">
-										<div data-icon="&#xe083;" className="icon"></div>
-									</div>
 									<h2>{this.state.Titulo}</h2>
 								</div>
 								<p>{this.state.parrafo}</p>
-								<div className="button service_item_button trans_200">
-									<label className="trans_200">{this.state.boton}</label>
-								</div>
 							</div>
 						</div>
 					</div>
