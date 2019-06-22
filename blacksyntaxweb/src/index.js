04fda6e1a9797ee7b9edfbd61c86e60a9596d23b
Praxx14 <samgrerojo@gmail.com>
2019-06-22 11:25:02 -0500

    Pedro v 4

--- blacksyntaxweb/src/index.js
+++ blacksyntaxweb/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// mandatory
+import 'react-s-alert/dist/s-alert-default.css';
 import 'react-s-alert/dist/s-alert-css-effects/slide.css';
 import 'react-s-alert/dist/s-alert-css-effects/scale.css';
 import 'react-s-alert/dist/s-alert-css-effects/bouncyflip.css';
@@ -10,6 +11,9 @@ import 'react-s-alert/dist/s-alert-css-effects/flip.css';
 import 'react-s-alert/dist/s-alert-css-effects/genie.css';
 import 'react-s-alert/dist/s-alert-css-effects/jelly.css';
 import 'react-s-alert/dist/s-alert-css-effects/stackslide.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
