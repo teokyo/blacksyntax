18ac07df46ca393f35e3de46be7b8ef144021242
Praxx14 <samgrerojo@gmail.com>
2019-06-22 09:29:04 -0500

    Pedro Reportes  v2

--- blacksyntaxweb/src/Componentes/Reportes.js
+++ blacksyntaxweb/src/Componentes/Reportes.js
@@ -1,110 +1,94 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 import "../App.css";
 import "../css/Reportes.css";
 
 class Reportes extends Component {
-  render() {
-    return (
-      <div className="reportes">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-12" id="reporteTitle">
-              <h1>Filtros de reporte</h1>
-            </div>
-          </div>
+    state = {FechaInical: '', FechaFinal: ''};
 
-<<<<<<< HEAD
-          <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-6">
-              <form>
-                <span>Por rango de fechas</span>
-                <br/>
-                <span>De: </span>
-                <input type="date" placeholder="dd/mm/aaaa" />
-                <br />
-                <span> A: </span>
-                <input type="date" placeholder="dd/mm/aaaa" />
-              </form>
-            </div>
-            <div className="col-sm-12 col-md-12 col-lg-6">
-              <form>
-=======
-          <form>
-            <div className="row">
-              <div className="col-sm-12 col-md-6 col-lg-4">
-                <span>Por rango de fechas</span>
-                <br />
-                <span>De: </span> <br />
-                <input type="date" placeholder="dd/mm/aaaa" />
-                <br />
-                <span> A: </span> <br />
-                <input type="date" placeholder="dd/mm/aaaa" />
-              </div>
-              <div className="col-sm-12 col-md-6 col-lg-4">
->>>>>>> Joel
-                <span>Por código de producto: </span>
-                <br />
-                <input type="text" />
-              </div>
+    componentWillMount() {
 
-              <div className="col-sm-12 col-md-6 col-lg-4">
-                <span>Por fecha de caducidad:</span>
-                <br />
-                <input type="date" />
-              </div>
-            </div>
-<<<<<<< HEAD
-          </div>
+    }
 
-          <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-6">
-              <form>
-                <span>Por fecha de caducidad:</span>
-                <br />
-                <input type="date" />
-              </form>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-sm-12 col-md-12 col-lg-12">
-              <a>Realizar reporte</a>
-            </div>
-          </div>
-=======
+    Cambio = (e) => {
+        const name = e.target.id;
+        const value = e.target.value;
+        this.setState({[name]: value}, () => console.log(this.state[name]))
+    };
 
-            <div className="row">
-              <div className="col-sm-12 col-md-6 col-lg-4">
-                <span>Ordenar por productos por:</span>
-                <br />
-                <input type="radio" name="orden" value="masvendido" /> Más
-                vendidos<br />
-                <input type="radio" name="orden" value="menosvendido" />
-                Menos vendidos
-              </div>
+    render() {
+        return (
+            <div className="reportes">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-lg-12" id="reporteTitle">
+                            <h1>Filtros de reporte</h1>
+                        </div>
+                    </div>
+                    <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
 
-              <div className="col-sm-12 col-md-6 col-lg-4">
-                <span>Por categoría</span>
-                <br />
-                <select>
-                  <option>hola</option>
-                  <option>hola</option>
-                  <option>hola</option>
-                </select>
-              </div>
-            </div>
+                        <form>
+                            <div className="row">
+                                <div className="col-sm-12 col-md-6 col-lg-4">
+                                    <span>Por rango de fechas</span>
+                                    <br/>
+                                    <span>De: </span> <br/>
+                                    <input value={this.state.FechaInical} onChange={this.Cambio} type="date"
+                                           className="form-control" placeholder="Usuario"/>
+                                    <br/>
+                                    <span> A: </span> <br/>
+                                    <input id='FechaFinal' value={this.state.FechaFinal} onChange={this.Cambio}
+                                           type="date" className="form-control" placeholder="dd/mm/aaaa"/>
+                                </div>
+                                <div className="col-sm-12 col-md-6 col-lg-4">
 
-            <div className="row">
-              <div className="col-sm-12" id="repcontent">
-                <a className="button-reporte" href="#" >
-                  Realizar reporte
-                </a>
-              </div>
+                                    <span>Por código de producto: </span>
+                                    <br/>
+                                    <input id='CodigoProducto' onClick={this.Cambio} type="text"/>
+                                </div>
+
+
+                            </div>
+
+                            <div className="row">
+                                <div className="col-sm-12 col-md-6 col-lg-4">
+                                    <span>Ordenar por productos por:</span>
+                                    <br/>
+                                    <input id='OrderVenta' onClick={this.Cambio} type="radio" name="orden"
+                                           value="masvendido"/> Más
+                                    vendidos<br/>
+                                    <input id='OrderVenta' onClick={this.Cambio} type="radio" name="orden"
+                                           value="menosvendido"/>
+                                    Menos vendidos
+                                </div>
+
+                                <div className="col-sm-12 col-md-6 col-lg-4">
+                                    <span>Por categoría</span>
+                                    <br/>
+                                    <select id='Category' value={this.state.Category} onChange={this.Cambio}>
+                                        <option value=''></option>
+                                        <option value='Hola 1'>hola</option>
+                                        <option value='Hola 2'>hola</option>
+                                    </select>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-sm-12" id="repcontent">
+                                    <button className="button-reporte">
+                                        Realizar reporte
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div className='col-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+
+                    </div>
+
+                </div>
             </div>
-          </form>
->>>>>>> Joel
-        </div>
-      </div>
-    );
-  }
+        );
+    }
 }
+
 export default Reportes;
